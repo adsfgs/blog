7d8b241452f6d0d7daeefaad5f7a07f5f63114bb
etalkr <etalkr@gmail.com>
2018-07-28 14:22:53 +0800

    Add files via upload

---
+++ tool/batchurl/Search.bat
@@ -0,0 +1,11 @@
+@echo off
+for /f "tokens=*" %%a in ('more %1') do (start chrome -incognito "https://www.baidu.com/s?wd=%%a")
+
+
+:: Comments for more Examples:
+:: start iexplore "%%a"
+:: start iexplore -private "http://www.site.com/query=%%a"
+:: start firefox -private "http://www.site.com/query=%%a"
+:: start chrome -incognito "http://www.site.com/query=%%a"
+:: start chrome -incognito "http://www.site.com/query1=%%a" "http://www.site.com/query2=%%a"
+
